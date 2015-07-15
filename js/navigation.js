ec6218ec1c0d09c7b79fbb31add46b69879979a5
Kiril Chitinov <k.chitinov@gmail.com>
2015-07-15 15:52:57 +0300

    Convert tabs to spaces

--- js/navigation.js
+++ js/navigation.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   showMenu();
 
   function showMenu(){
-  	$('.menu-trigger').on('click', function(){
-  		$('#main-menu').slideToggle();
-  	});
+    $('.menu-trigger').on('click', function(){
+      $('#main-menu').slideToggle();
+    });
   }
 });
