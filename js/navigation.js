91080869b000def61f3852814a7a361f98fd2c04
Kiril <Kiril@macpro060s-MacBook-Pro.local>
2015-09-09 15:58:15 +0300

    Fix main-menu disappearing in tablet and desktop views

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,17 @@
-$(document).ready(function() {
+function init() {
+	$(window).resize(onResize);
+
   $('.menu-trigger').on('click', function(){
     $('#main-menu').slideToggle();
   });
+}
+
+function onResize() {
+	if ( $(window).width() > 768 && !$('.menu-trigger').is(':visible') ) {
+		$('#main-menu').show();
+	}
+}
+
+$(document).ready(function() {
+	init();
 });
